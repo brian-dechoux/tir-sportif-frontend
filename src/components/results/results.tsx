c0ffe99dabb347742d22d87649b92610793f52c1
Brian Dechoux <brian.dechoux@intech.lu>
2020-01-08 13:53:23 +0100

    some work

--- src/components/results/results.tsx
+++ src/components/results/results.tsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import AuthenticatedContainer from '../authenticated-route/authenticated.container';
 import { Typography } from '@material-ui/core';
-import Layout from '../layout/layout';
+import Menu from '../menu/menu';
 import HeaderContainer from '../header/header.container';
 
 const Results = () =>  {
   return (
     /*<AuthenticatedContainer mustRedirect={false}>
-      <Layout>
+      <Menu>
         <Typography>Results</Typography>
-      </Layout>
+      </Menu>
     </AuthenticatedContainer>*/
     <>
 
