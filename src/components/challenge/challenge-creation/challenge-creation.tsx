cce0873110a0337b94f0fcc77afc8b13f10dddd5
Brian Dechoux <brian.dechoux@intech.lu>
2020-04-23 17:19:34 +0200

    Validation form input and datepicker

--- src/components/challenge/challenge-creation/challenge-creation.tsx
+++ src/components/challenge/challenge-creation/challenge-creation.tsx
@@ -1,12 +1,7 @@
 import React, { useState } from 'react';
 import DateFnsUtils from '@date-io/date-fns';
 import { Box, Button, FormControl, Grid, TextField, Typography } from '@material-ui/core';
-import ScheduleIcon from '@material-ui/icons/Schedule';
-import {
-  KeyboardDatePicker,
-  KeyboardTimePicker,
-  MuiPickersUtilsProvider,
-} from '@material-ui/pickers';
+import { DateTimePicker, KeyboardDateTimePicker, MuiPickersUtilsProvider } from '@material-ui/pickers';
 import { ThunkAction } from 'redux-thunk';
 import { AppState } from 'redux/reducers/combined.reducer';
 import { CreateAddressRequest } from 'services/models/address.model';
@@ -28,16 +23,12 @@ type ChallengeCreationProps = {
   };
 };
 
-type ValidationErrors = {
-  challengeNameTextFieldError: boolean;
-};
-
 const ChallengeCreation = (props: ChallengeCreationProps) => {
   const [challengeNameValid, setChallengeNameValid] = useState(true);
-  const [selectedDate, setSelectedDate] = useState<Date | null>(new Date());
+  const [selectedDateTime, setSelectedDateTime] = useState<Date | null>(null);
   const [inputName, setName] = useState('');
 
-  const formValid = ![challengeNameValid].some(validation => !validation);
+  const formValid = ![challengeNameValid, !!selectedDateTime].some(validation => !validation);
 
   const handleNameChange = (event: any) => {
     const newValue = event.target.value;
@@ -51,10 +42,12 @@ const ChallengeCreation = (props: ChallengeCreationProps) => {
       city: 'Val de Briey',
       countryId: 74,
     };
+
+    // FIXME BDX throw if null date
     props.actions.createChallenge(
       inputName,
       address,
-      selectedDate ? selectedDate : new Date(),
+      selectedDateTime ? selectedDateTime : new Date(),
       1,
       [1],
       [1]
@@ -66,11 +59,11 @@ const ChallengeCreation = (props: ChallengeCreationProps) => {
       <form onSubmit={handleChallengeCreation} noValidate>
         <Box display="flex" justifyContent="center">
           <Box display="flex" width={0.6}>
-            <Grid container spacing={3}>
+            <Grid container spacing={3} alignItems="center">
               <Grid item xs={12}>
                 <Typography variant="h6">CRÉER UN CHALLENGE</Typography>
               </Grid>
-              <Grid item md={12}>
+              <Grid item xs={12}>
                 <TextField
                   error={!challengeNameValid}
                   fullWidth
@@ -79,38 +72,19 @@ const ChallengeCreation = (props: ChallengeCreationProps) => {
                   onChange={handleNameChange}
                 />
               </Grid>
-              <Grid item md={6}>
-                <FormControl fullWidth required>
-                  <KeyboardDatePicker
-                    clearable
+              <Grid item xs={12}>
+                <FormControl fullWidth>
+                  <DateTimePicker
+                    required
                     disablePast
-                    placeholder="10/10/2010"
-                    format="dd/MM/yyyy"
-                    margin="normal"
-                    id="date-picker"
-                    label="Date de début"
-                    value={selectedDate}
-                    onChange={setSelectedDate}
-                    clearLabel={datePickerLabels.clearLabel}
-                    cancelLabel={datePickerLabels.cancelLabel}
-                    okLabel={datePickerLabels.okLabel}
-                    todayLabel={datePickerLabels.todayLabel}
-                    invalidDateMessage={datePickerLabels.invalidDateMessage}
-                  />
-                </FormControl>
-              </Grid>
-              <Grid item md={6}>
-                <FormControl fullWidth required>
-                  <KeyboardTimePicker
-                    clearable
-                    placeholder="10:10"
+                    placeholder="10/10/2010 10:10"
+                    format="dd/MM/yyyy hh:mm"
                     margin="normal"
                     ampm={false}
-                    id="time-picker"
-                    label="Heure de début"
-                    value={selectedDate}
-                    onChange={setSelectedDate}
-                    keyboardIcon={<ScheduleIcon />}
+                    id="datetime-picker"
+                    label="Date et heure du challenge"
+                    value={selectedDateTime}
+                    onChange={setSelectedDateTime}
                     clearLabel={datePickerLabels.clearLabel}
                     cancelLabel={datePickerLabels.cancelLabel}
                     okLabel={datePickerLabels.okLabel}
