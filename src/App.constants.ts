3138b65a6ee520a561bb22abcbc6554848231b92
Brian Dechoux <brian.dechoux@intech.lu>
2020-08-05 14:33:40 +0200

    clean constant

--- src/App.constants.ts
+++ src/App.constants.ts
@@ -13,8 +13,3 @@ export const NA = 'N/A';
 export const REGEXES = {
   EMAIL: /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
 }
-
-// FIXME Use proper useMediaQuery Hook from library
-export const MEDIA = {
-  DESKTOP: 992
-}
